7b4032e7450367711d3b010d327eaf8dcf541b44
haolun1996 <33619117+haolun1996@users.noreply.github.com>
2025-04-02 18:00:47 +0000

    Deploying to gh-pages from @ haolun1996/plot-on-side-plan@c88eb3c0412cd905c15dfa41bf73a194a8f174d0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3700615281"
+    serviceWorkerVersion: "3314544931"
   }
 });
